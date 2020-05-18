8f650c5fa50f5842581fa40861a97876a27fbc7a
Filipe m Silva <flpms@MacBook-Pro-de-Filipe.local>
2020-05-18 18:22:08 -0300

    exemplo ruim

--- es2015-bad-example/car.test.js
+++ es2015-bad-example/car.test.js
@@ -4,7 +4,7 @@ describe('Teste da unidade Car', () => {
   const car = new Car();
     
   it('espera que a propriedade velocity não mude quando chamar moveFoward e sem a propriedade batteryLevel', () => {
-      const  = car.velocity;
+      const initialVelocity = car.velocity;
       car.moveFoward();
       assert.equal(car.velocity, initialVelocity);
   });
